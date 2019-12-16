c589b763f4d41d01fa0560c411b628b5e64f270d
EXC46189 <julian.markowskyj@claro.com.ar>
2019-12-15 23:34:49 -0300

    Initial commit

---
+++ src/test/java/com/project/indicators/IndicatorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.indicators;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndicatorsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
