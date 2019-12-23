86e3a52ef37908b6a7607ce176bc399b0822ead9
unknown <sanchezdanielacv@gmail.com>
2019-12-22 21:20:37 -0300

    Se elimina service OSC

--- src/main/java/com/project/indicators/service/IndicatorsService.java
+++ src/main/java/com/project/indicators/service/IndicatorsService.java
@@ -2,12 +2,9 @@ package com.project.indicators.service;
 
 import com.project.indicators.builder.HistoricalBuilder;
 import com.project.indicators.mapper.HistoricalMapper;
-import com.project.indicators.mapper.OSCMapper;
-import com.project.indicators.model.OSC;
 import com.project.indicators.model.dto.HistoricalDTO;
 import com.project.indicators.service.http.HistoricalRequest;
 import com.project.indicators.service.http.HistoricalResponse;
-import com.project.indicators.service.http.OSCsResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,31 +12,17 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 public class IndicatorsService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(IndicatorsService.class);
-    private final OSCMapper oscMapper;
     private final HistoricalMapper historicalMapper;
     private final HistoricalBuilder historicalBuilder;
 
     @Autowired
-    public IndicatorsService(OSCMapper oscMapper, HistoricalMapper historicalMapper, HistoricalBuilder historicalBuilder) {
-        this.oscMapper = oscMapper;
+    public IndicatorsService( HistoricalMapper historicalMapper, HistoricalBuilder historicalBuilder) {
         this.historicalMapper = historicalMapper;
         this.historicalBuilder = historicalBuilder;
-    }
-
-    @GetMapping(
-        value = "employee/indicators/osc/{idEmployee}",
-        produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<OSCsResponse> obtainOSCs(@PathVariable Integer idEmployee) {
-        final List<OSC> oscList = oscMapper.obtainOSCs(idEmployee);
-        LOGGER.info("Se obtuvieron las oscs para el empleado "+idEmployee);
-        return ResponseEntity.ok( new OSCsResponse(oscList));
-
     }
 
     @PostMapping(
